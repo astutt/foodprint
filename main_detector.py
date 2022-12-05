6cca423707874209f783992b4123eaecebe87e45
Alex Stutt <alexandra.stutt@gmail.com>
2022-12-04 21:24:32 -0500

    clean up main and organize pics

--- main_detector.py
+++ main_detector.py
@@ -2,8 +2,15 @@ import torch
 import utils
 display = utils.notebook_init()  # checks
 
-def get_fruit_text(img_path):
-  detected = !python detect.py --weights yolov5s.pt --img 640 --conf 0.25 --source img_path
-  return detected
+img_path = './test/apple.jpeg'
 
-get_fruit_text()
+detected = !python detect.py --weights yolov5s.pt --img 640 --conf 0.25 --source img_path
+
+print(detected)
+
+# def get_fruit_text(img_path):
+#   detected = !python detect.py --weights yolov5s.pt --img 640 --conf 0.25 --source img_path
+#   return detected
+
+# get_fruit_text()
+
