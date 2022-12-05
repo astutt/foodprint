2ce35f870d41004f52296dc9edae755ed34a70bf
tjt22 <lenwortt@andrew.cmu.edu>
2022-12-05 01:18:09 -0500

    GUI with updated elements

---
+++ Emissions Calculator.py
@@ -0,0 +1,35 @@
+
+
+apple_kg = 0.15
+apple_co2 = 0.3
+banana_kg = 0.12
+banana_co2 = 0.8
+citrus_kg = 0.14
+citrus_co2 = 0.3
+carrot_kg = 0.02
+carrot_co2 = 0.3
+broccoli_kg = 0.4
+broccoli_co2 = 0.4
+
+co2_total = 0
+
+detected = {'apples': 2, 'bananas': 2, 'oranges': 2, 'carrots':2, 'broccoli':2}
+
+if 'apples' in detected:
+    apple_em = detected['apples']*apple_kg*apple_co2
+    co2_total += apple_em
+elif 'bananas' in detected:
+    banana_em = detected['bananas']*banana_kg*banana_co2
+    co2_total += banana_em
+elif 'oranges' in detected:
+    orange_em = detected['oranges']*citrus_kg*citrus_co2
+    co2_total += orange_em
+elif 'carrots' in detected:
+    carrot_em = detected['carrots']*carrot_kg*carrot_co2
+    co2_total += carrot_em
+elif 'broccoli' in detected:
+    broccoli_em = detected['broccoli']*broccoli_kg*broccoli_co2
+    co2_total += broccoli_em
+
+print(co2_total)
+print("kg of CO2")
--- curtain_demo.py
+++ curtain_demo.py
@@ -5,65 +5,115 @@ import PIL.Image
 
 import datetime
 
-from PIL import Image 
+from PIL import Image
+
+from geopy.geocoders import Nominatim
+from pprint import pprint
+import time
+import geocoder
+import pandas as pd
+import numpy as np
+
+g = geocoder.ip('me')
+
+app = Nominatim(user_agent="tutorial")
+
+def get_address_by_location(latitude, longitude, language="en"):
+    """This function returns an address as raw from a location
+    will repeat until success"""
+    # build coordinates string to pass to reverse() function
+    coordinates = f"{latitude}, {longitude}"
+    # sleep for a second to respect Usage Policy
+    time.sleep(1)
+    try:
+        return app.reverse(coordinates, language=language).raw
+    except:
+        return get_address_by_location(latitude, longitude)
+
+# define your coordinates
+latitude = g.latlng[0]
+longitude = g.latlng[1]
+# get the address info
+address = get_address_by_location(latitude, longitude)
+# print all returned d
+#print(address)
+current_city = address['address']['city']
 
 # Main page: Inputs
+# shortcodes :apple::earth_americas::shopping_trolley:
 
-st.title("Welcome to FoodPrint!") 
+st.title("Welcome to FoodPrint!:apple::shopping_trolley::earth_americas:")
 
 st.subheader("This application calculates the total carbon footprint of your foods in your grocery cart and provides food substitutions to reduce carbon footprint.")
-
-image = Image.open('carbonemission.jpg')
-
-st.image(image, caption = None, width = 210, use_column_width = 210) 
-
 
 
 # sidebar inputs
 
-image_two = Image.open('welcome.jpg')
-
-st.sidebar.image(image_two, caption = None, width = 145, use_column_width = 145) 
+image = Image.open(r"C:\Users\ltjth\Documents\GitHub\foodprint\image-removebg-preview (18).png")
+st.sidebar.image(image, caption = None, width = 210, use_column_width = 210)
 
 name = st.sidebar.text_input("What is your name?")
 
-output_name = st.sidebar.write("Hi" + " " +  str(name) + "," +  " " + "welcome to FootPrint! We will help you calculate the carbon footprint of your food.")
-
-st.sidebar.text_input("Please enter your location:")
-
+output_name = st.sidebar.write("Hi" + " " +  str(name) + "," +  " " + "welcome to FoodPrint! We will help you calculate the carbon footprint of your food.")
 today = datetime.date.today()
+st.sidebar.markdown('**Time and Location:**')
+st.sidebar.write(current_city)
+st.sidebar.markdown('**Current Date:**')
+st.sidebar.write(str(today))
+#st.sidebar.write(current_city)
 
-date = st.sidebar.write("Current Date: " + str(today)) 
 
-str_date = str(today)
+
+
+#date = st.sidebar.write("Current Date: " + str(today))
+
+#str_date = str(today)
 
 my_image = st.sidebar.file_uploader("Upload an image of your grocery cart below:", type = ["png", "jpg", "jpeg"])
 
 if my_image is not None:
-    
     image = Image.open(my_image)
+    st.sidebar.image(image)
 
-st.sidebar.button('Calculate total carbon emissions')
+if st.sidebar.button('Calculate total carbon emissions'):
+    my_bar = st.progress(0)
+    for percent_complete in range(100):
+        time.sleep(0.001)
+        my_bar.progress(percent_complete + 1)
+    st.success('Thanks for doing your part!', icon="✅")
+    tab1, tab2 = st.tabs(["Results", "Tips"])
+    img2 = Image.open(r"C:\Users\ltjth\Documents\GitHub\foodprint\example pictures\example annotated_camila.png")
+    with tab1:
+        col1, col2 = st.columns(2)
+        with col1:
+            d = {'Item': ["Apples", "Oranges", "Carrots"], "Quantity": [1, 4, 2], "Emissions (kg)": [0.02, 1, .98]}
+            #st.subheader("Estimated carbon emissions of your grocery cart: ")
+            st.metric(label = "Co2 Emissions", value="2 kg Co2", delta="GOOD")
+            st.write("We detected: ")
+            df = pd.DataFrame(data=d)
+            st.table(df)  # Same as st.write(df)
+        with col2:
+            st.image(img2)
+    with tab2:
+        st.header("Tips to Reduce Your Food's Carbon Emissions")
+        st.subheader("- Eat produce that is in season")
+        st.subheader("- Buy Local")
+        st.subheader('- Decrease your red meat intake')
+        st.markdown('[Learn More](https://ourworldindata.org/food-choice-vs-eating-local "Learn More")', unsafe_allow_html=False)
 
+    # Step 1: Display the month name based on current date
 
+    # month_data = datetime.datetime.strptime(str_date, "%Y-%m-%d")
+    # num_month = st.sidebar.write(month_data.month)
+
+    # Extracting the month (ex. current date: 2022-12-04 -> outputs: December )
+    month_gen = today.strftime("%B")
+
+    # Step 2: Upload csv file to use vlookup on seasons vs. food
 
 # Output Total Carbon Emissions
 # Unit: kg C02
 
-st.write("Total carbon emissions of your grocery cart:  ")
-
-st.write("Food Substitutions: ")
-
-# Step 1: Display the month name based on current date
-
-#month_data = datetime.datetime.strptime(str_date, "%Y-%m-%d")
-#num_month = st.sidebar.write(month_data.month)
-
-#Extracting the month (ex. current date: 2022-12-04 -> outputs: December )
-month_gen= today.strftime("%B")
-
-
-# Step 2: Upload csv file to use vlookup on seasons vs. food
 
 
 
