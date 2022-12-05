d4fe35b1965489f4e08e881478762dab6a95eb38
tjt22 <114026856+tjt22@users.noreply.github.com>
2022-12-04 20:51:50 -0500

    Forgot to add a return statement

--- main_detector.py
+++ main_detector.py
@@ -3,4 +3,7 @@ import utils
 display = utils.notebook_init()  # checks
 
 def get_fruit_text(img_path):
-    !python detect.py --weights yolov5s.pt --img 640 --conf 0.25 --source img_path
+  detected = !python detect.py --weights yolov5s.pt --img 640 --conf 0.25 --source img_path
+  return detected
+
+get_fruit_text()
